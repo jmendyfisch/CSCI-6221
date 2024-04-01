80dcbc628f373c0f455ba1ee049da098f31d5f8a
jmendyfisch <145626546+jmendyfisch@users.noreply.github.com>
2024-04-01 01:07:38 -0400

    adding the intake page which currently features a recorder which saves recordings to the tempaudio file.  however for some reason the audio recording is not picking up any sound. it is saving files of the correct length (if you look in the tempaudio folder that is being created within the docker) but no sound is being recorded.

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"server/controller"
 	"server/database"
 	"server/service"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +17,9 @@ func main() {
 	database.Init()
 
 	router := gin.Default()
+
+	router.Static("/templates", "./templates")
+
 	serv := service.New()
 	cont := controller.New(serv)
 
@@ -28,9 +32,33 @@ func main() {
 // Sets all the endpoints for the Gin router
 func SetEndpoints(r *gin.Engine, c *controller.Controller) {
 
-	r.LoadHTMLFiles("templates/index.html")
+	r.LoadHTMLFiles("templates/index.html", "templates/intake.html")
+
+	r.POST("/save-audio", func(ctx *gin.Context) {
+		file, err := ctx.FormFile("audio")
+		if err != nil {
+			ctx.String(http.StatusBadRequest, "Get form err: %s", err.Error())
+			return
+		}
+
+		// Generate a unique filename, e.g., using a timestamp
+		filename := fmt.Sprintf("recording_%v.wav", time.Now().UnixNano())
+		path := fmt.Sprintf("/tempaudio/%s", filename)
+
+		// Save the uploaded file
+		if err := ctx.SaveUploadedFile(file, path); err != nil {
+			ctx.String(http.StatusInternalServerError, "Save uploaded file err: %s", err.Error())
+		} else {
+			ctx.String(http.StatusOK, "File uploaded successfully: %s", filename)
+		}
+	})
+
 	r.GET("", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	r.GET("/intake", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "intake.html", gin.H{})
 	})
 
 	r.GET("/cases", func(ctx *gin.Context) {
